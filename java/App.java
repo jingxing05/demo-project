4ddb4061858ae271b757bf38f7673754c9d63bdd
jingxing05 <jingxing05@163.com>
2013-11-28 21:38:31 +0800

    message

--- java/App.java
+++ java/App.java
@@ -4,9 +4,7 @@ public class App{
     
        System.out.println("App running ...");  
     
-       System.out.println("App stop ...");
-       
-       System.out.println("App delete by Obama :)");
+       System.out.println("App stop by Obama..."); 
         
     }  
 } 
