c262c6fd3f3bb3605235b1f881a627e87f5c4513
jingxing05 <jingxing05@163.com>
2013-11-28 21:36:15 +0800

    add

--- java/App.java
+++ java/App.java
@@ -5,6 +5,8 @@ public class App{
        System.out.println("App running ...");  
     
        System.out.println("App stop ...");
+       
+       System.out.println("App delete by Obama :)");
         
     }  
 } 
